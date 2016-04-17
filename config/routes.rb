9dab785da9db3eaed5ed7deb0ded5e337814ef71
Anna Natorilla <annagaiel@gmail.com>
2016-04-16 21:49:04 -0700

    show individual product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "products#index"
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show'
+
+  # resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

cce27d2baf3d46805975f0cae8b083b3a98d0621
Anna Natorilla <annagaiel@gmail.com>
2016-04-18 20:27:28 -0700

    Move the New product link, move the edit and delete below the table

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,22 @@ class Product < ActiveRecord::Base
   validates :price, presence: true
   validates :image, presence: true
   validates :description, presence: true
+
+  DISCOUNT_THRESHOLD = 2
+
+  def sale_message
+    price < DISCOUNT_THRESHOLD ? "Discount Item" : "On Sale!"
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
+  def color_price
+    price < DISCOUNT_THRESHOLD ? "green" : "red"
+  end
 end

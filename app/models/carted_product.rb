d3ef2688ce746e586042b7d13b9a793977c4905f
Anna Natorilla <annagaiel@gmail.com>
2016-05-04 19:20:42 -0700

    Add custom validation on CartedProduct

--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,4 +1,12 @@
 class CartedProduct < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+  validates :quantity_must_be_less_than_5
+
+  def quantity_must_be_less_than_5
+    if quantity && quantity > 4
+      errors.add(:quantity, "You can only have up to 4 products")
+    end
+  end
 end
